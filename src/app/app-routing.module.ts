cb49fb397dd4f6ccec2fbf5f9575883eeb13abef
Arina Matushkina <arina1998-v2013@yandex.ru>
2023-04-25 08:54:36 +0300

    clear comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,7 @@ const routes: Routes = [
         import('./news-list/news-list.module').then(m => m.NewsListModule),
     },
   ]),
-  // Shell.childRoutes([
-  //   {
-  //     path: 'employee',
-  //     // canActivate:[AuthGuardWithForcedLogin],
-  //     loadChildren: () => import('./features/employee/employee.module').then((m) => m.EmployeeModule),
-  //   },
-  //   {
-  //     path: 'position',
-  //     loadChildren: () => import('./features/position/position.module').then((m) => m.PositionModule),
-  //   },
 
-  // ]),
-  // { path: 'should-login', component: ShouldLoginComponent },
-
-  // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
